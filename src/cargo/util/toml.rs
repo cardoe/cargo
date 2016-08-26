e655012c365a4bad65763d80a218e27c694adecb
John Ericson <Ericson2314@Yahoo.com>
2016-08-26 15:34:16 -0700

    deps-parsing tiny refeactor & reformat

--- src/cargo/util/toml.rs
+++ src/cargo/util/toml.rs
@@ -576,18 +576,14 @@ impl TomlManifest {
                 layout: &layout,
             };
 
-            fn process_dependencies(
+            fn process_deps(
                 cx: &mut Context,
                 new_deps: Option<&HashMap<String, TomlDependency>>,
                 allow_explicit_stdlib: bool,
                 kind: Option<Kind>)
                 -> CargoResult<()>
             {
-                let dependencies = match new_deps {
-                    Some(ref dependencies) => dependencies,
-                    None => return Ok(())
-                };
-                for (n, v) in dependencies.iter() {
+                for (n, v) in new_deps.iter().flat_map(|t| *t) {
                     let detailed = v.elaborate();
                     if let Some(true) = detailed.stdlib {
                         if !allow_explicit_stdlib {
@@ -604,21 +600,27 @@ impl TomlManifest {
             }
 
             // Collect the deps
-            process_dependencies(&mut cx, self.dependencies.as_ref(),
-                                 None)?;
-            process_dependencies(&mut cx, self.dev_dependencies.as_ref(),
-                                 Some(Kind::Development))?;
-            process_dependencies(&mut cx, self.build_dependencies.as_ref(),
-                                 Some(Kind::Build))?;
+            process_deps(
+                &mut cx, self.dependencies.as_ref(),
+                true, None)?;
+            process_deps(
+                &mut cx, self.dev_dependencies.as_ref(),
+                false, Some(Kind::Development))?;
+            process_deps(
+                &mut cx, self.build_dependencies.as_ref(),
+                false, Some(Kind::Build))?;
 
             for (name, platform) in self.target.iter().flat_map(|t| t) {
                 cx.platform = Some(name.parse()?);
-                process_dependencies(&mut cx, platform.dependencies.as_ref(),
-                                     None)?;
-                process_dependencies(&mut cx, platform.build_dependencies.as_ref(),
-                                     Some(Kind::Build))?;
-                process_dependencies(&mut cx, platform.dev_dependencies.as_ref(),
-                                     Some(Kind::Development))?;
+                process_deps(
+                    &mut cx, platform.dependencies.as_ref(),
+                    true, None)?;
+                process_deps(
+                    &mut cx, platform.build_dependencies.as_ref(),
+                    false, Some(Kind::Build))?;
+                process_deps(
+                    &mut cx, platform.dev_dependencies.as_ref(),
+                    false, Some(Kind::Development))?;
             }
 
             replace = self.replace(&mut cx)?;
